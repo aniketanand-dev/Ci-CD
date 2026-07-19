4135d38f423814625b04b3591fc38b3323a5f3d3
Aniket72094087100 <aniket72094087100@gmail.com>
2026-07-19 14:41:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("hello 12")
+});
+
+
+app.listen('3000', () => {
+    console.log("Server start at: 3000")
+})
